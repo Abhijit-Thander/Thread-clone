47051d7feb0af2705c593c65328f581a2124c96c
Abhijit <abhijitthander38@gmail.com>
2025-05-02 23:44:22 +0530

    Types added

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,16 @@
-
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View>
-      <Text className='text-red-500 text-5xl'>Feed</Text> 
-     <StatusBar style='light' />  
+      <Text className='text-white'>Welcome to Threads</Text>
+      <StatusBar style='light' />  
     </View>
   );
 }
+
+
+
 
 
---
+++ src/types.ts
@@ -0,0 +1,21 @@
+export type User = {
+    id: string;
+    username: string;
+    image: string;
+    name: string;
+    bio: string;
+}
+
+export type Post = {
+    id: string;
+    createdAt: string;
+    content: string;
+    user: User;
+    user_id: string;
+    parent_id: string | null;
+    parent: Post | null;
+    replies: Post[];
+  
+    
+
+}
